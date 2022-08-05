3186129792ab8bfe6200a78f8f665848e64eceaa
Boun13a <mo.halbouni@gmail.com>
2022-08-05 13:23:23 -0400

    initial commit

---
+++ HandyApp_V2/app/src/main/java/Activities/models/Rate.java
@@ -0,0 +1,14 @@
+package Activities.models;
+
+public class Rate {
+    String rating;
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+    
+}
